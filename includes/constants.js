caea4cfb6348f62f62fad78a3bbbf18d4cb9beed
Raphael Miranda <raphael.miranda@rioonibus.com>
2024-12-11 17:24:47 +0000

    Criado shapes_geom_planejamento e aux_shapes_geom_filtrada.

--- includes/constants.js
+++ includes/constants.js
@@ -1,19 +1,13 @@
-let data_versao_manual = new Date("2024-12-09").toISOString().split('T')[0]; // -- reprocessamento setar a data aqui
-let data_versao_gtfs = new Date().toISOString().split('T')[0]; // -- fixada última data disponível
-
-if (data_versao_manual != "") {
-  data_versao_gtfs = `"${data_versao_manual}"`;
+function get_last_feed_start_date() {
+  return `
+    (
+    SELECT 
+      MAX(feed_start_date) AS max_date 
+    FROM \`rj-smtr.gtfs.feed_info\`
+    )
+  `;
 }
 
-function get_last_feed_start_date(ctx) {
-  if (ctx.incremental()) {
-    return `
-      SELECT MAX(feed_start_date) FROM ${ref("gtfs", "feed_info")} WHERE feed_start_date < '${data_versao_gtfs}'`;
-  } else {
-    return `${data_versao_gtfs}`; 
-  }
-}
+const data_versao_gtfs = get_last_feed_start_date()
 
-module.exports = { data_versao_gtfs, get_last_feed_start_date };
-
-
+module.exports = { data_versao_gtfs };
