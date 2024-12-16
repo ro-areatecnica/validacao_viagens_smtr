39ee6d4de0f020facb5fa61f0dd622724b2e572f
Raphael Miranda <raphael.miranda@rioonibus.com>
2024-12-16 20:41:53 +0000

    Ajuste nos datasets de monitoramento.

--- includes/constants.js
+++ includes/constants.js
@@ -1,6 +1,10 @@
 const buffer_tunel_metros = 50;
 const limite_reducao_area_buffer = 0.5;
 const comprimento_minimo_segmento_shape = 990;
+const date_range_start = `"2024-11-29T00:00:00"`; /*OBS: Verificar onde está passando estas variaveis*/
+const date_range_end = `"2024-11-29T00:59:59"`; /*OBS: Verificar onde está passando estas variaveis*/
+const data_inicial_gps_validacao_viagem = `"2024-10-11"`; /*OBS: Verificar onde está passando estas variaveis*/
+
 
 function get_last_feed_start_date() {
   return `
@@ -18,5 +22,8 @@ module.exports = {
   data_versao_gtfs, 
   buffer_tunel_metros, 
   limite_reducao_area_buffer, 
-  comprimento_minimo_segmento_shape
+  comprimento_minimo_segmento_shape,
+  date_range_start,
+  date_range_end,
+  data_inicial_gps_validacao_viagem
 };
