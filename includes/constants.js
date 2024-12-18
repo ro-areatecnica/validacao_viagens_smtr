9b86639a8d817e90d76be9da6c55174a6f517c69
Alex Perfeito <alex.perfeito@rioonibus.com>
2024-12-18 18:38:03 +0000

    Modificado a tabela shape_geom_planejamento e adicionado a variavel versao no arquivo constants

--- includes/constants.js
+++ includes/constants.js
@@ -1,10 +1,11 @@
+const versao = '';
 const buffer_tunel_metros = 50;
 const limite_reducao_area_buffer = 0.5;
 const comprimento_minimo_segmento_shape = 990;
-const date_range_start = `"2024-11-29T00:00:00"`; /*OBS: Verificar onde está passando estas variaveis*/
-const date_range_end = `"2024-11-29T00:59:59"`; /*OBS: Verificar onde está passando estas variaveis*/
-const data_inicial_gps_validacao_viagem = `"2024-10-11"`; /*OBS: Verificar onde está passando estas variaveis*/
 const parametro_validacao = 0.9;
+const data_inicial_gps_validacao_viagem = `"2024-07-17"`;
+const date_range_start = `"2024-07-17T00:00:00"`;
+const date_range_end = `"2024-07-17T00:59:59"`;
 
 function get_last_feed_start_date() {
   return `
@@ -12,6 +13,7 @@ function get_last_feed_start_date() {
     SELECT 
       MAX(feed_start_date) AS max_date 
     FROM \`rj-smtr.gtfs.feed_info\`
+    WHERE feed_start_date < data_inicial_gps_validacao_viagem
     )
   `;
 }
@@ -19,6 +21,7 @@ function get_last_feed_start_date() {
 const data_versao_gtfs = get_last_feed_start_date()
 
 module.exports = { 
+  versao,
   data_versao_gtfs, 
   buffer_tunel_metros, 
   limite_reducao_area_buffer, 
