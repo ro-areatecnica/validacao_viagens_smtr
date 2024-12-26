e6c0da3b51b5a6de6b907eeb3ffb26a79be185ef
Raphael Miranda <raphael.miranda@rioonibus.com>
2024-12-26 14:22:25 +0000

    Criação do arquivo helpers e ajuste no arquivo gps_viagem.

---
+++ includes/helpers.js
@@ -0,0 +1,11 @@
+const data_atual = new Date();
+
+data_atual.setDate(data_atual.getDate() - 1);
+
+const data_inicial = data_atual.toISOString().split('T')[0];
+
+const data_inicial_gps_validacao_viagem = `'${data_inicial}'`;
+
+module.exports = { data_inicial_gps_validacao_viagem };
+
+
